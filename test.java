f15897a21fa3c26ade62da635a999781301ffcb8
Santosh Koppisetti <2300032902@kluniversity.in>
2026-01-07 10:22:30 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
+        System.out.println("Hello World !! this sample file of java");
     }
 }
